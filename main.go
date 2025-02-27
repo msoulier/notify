c134ee9ff3cf0151db937476aeab8ca010e8ad3d
Michael P. Soulier <msoulier@digitaltorque.ca>
2025-02-27 18:07:22 -0500

    Ran a formatter

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-    "flag"
-    "os"
-    "github.com/gen2brain/beeep"
+	"flag"
+	"os"
+
+	"github.com/gen2brain/beeep"
 )
 
 var (
-    title string
-    message string
+	title   string
+	message string
 )
 
 func init() {
-    // FIXME: take stdin
-    flag.StringVar(&title, "title", "", "The notification title")
-    flag.StringVar(&message, "message", "", "The message text")
-    flag.Parse()
+	// FIXME: take stdin
+	flag.StringVar(&title, "title", "", "The notification title")
+	flag.StringVar(&message, "message", "", "The message text")
+	flag.Parse()
 
-    if title == "" || message == "" {
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
+	if title == "" || message == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 }
 
 func main() {
-    err := beeep.Notify(title, message, "")
-    if err != nil {
-        panic(err)
-    }
+	err := beeep.Notify(title, message, "")
+	if err != nil {
+		panic(err)
+	}
 }
